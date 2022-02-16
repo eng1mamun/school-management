e0467dd10eef7b260b13e5d05163e8b86ea79b47
eng1mamun <wub.mamun.cse@gmail.com>
2022-02-17 00:38:27 +0600

    first

---
+++ Controllers/WebSiteTempleteController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagementViewSite.Controllers
+{
+    public class WebSiteTempleteController : Controller
+    {
+        // GET: WebSiteTemplete
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Category()
+        {
+            return View("Category");
+        }
+        public ActionResult About()
+        {
+            return View("About");
+        }
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SchoolManagementViewSite.Startup))]
+namespace SchoolManagementViewSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
